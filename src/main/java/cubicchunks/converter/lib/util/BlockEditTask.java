b940d52558105ff5f5a681f78c8d85e55675ffc2
NotStirred <notstirred9@gmail.com>
2020-10-09 04:53:45 +0100

    Added replace comand

--- src/main/java/cubicchunks/converter/lib/util/BlockEditTask.java
+++ src/main/java/cubicchunks/converter/lib/util/BlockEditTask.java
@@ -2,21 +2,45 @@ package cubicchunks.converter.lib.util;
 
 public class BlockEditTask extends EditTask {
 
-    private final byte blockId;
-    private final byte blockMeta;
+    private final Byte inBlockId;
+    private final Byte inBlockMeta;
+
+    private final byte outBlockId;
+    private final byte outBlockMeta;
 
     public BlockEditTask(BoundingBox src, Vector3i offset, Type type, byte id, byte meta) {
         super(src, offset, type);
 
-        blockId = id;
-        blockMeta = meta;
+        inBlockId = null;
+        inBlockMeta = null;
+
+        outBlockId = id;
+        outBlockMeta = meta;
     }
 
-    public byte getBlockId() {
-        return blockId;
+    public BlockEditTask(BoundingBox src, Vector3i offset, Type type, byte inId, byte inMeta, byte outId, byte outMeta) {
+        super(src, offset, type);
+
+        inBlockId = inId;
+        inBlockMeta = inMeta;
+
+        outBlockId = outId;
+        outBlockMeta = outMeta;
     }
 
-    public byte getBlockMeta() {
-        return blockMeta;
+    public Byte getInBlockId() {
+        return inBlockId;
+    }
+
+    public Byte getInBlockMeta() {
+        return inBlockMeta;
+    }
+
+    public byte getOutBlockId() {
+        return outBlockId;
+    }
+
+    public byte getOutBlockMeta() {
+        return outBlockMeta;
     }
 }
