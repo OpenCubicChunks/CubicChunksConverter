e30ea3bb4a6f27603ec5f326a75474b557f4389a
NotStirred <notstirred9@gmail.com>
2020-10-22 17:48:50 +0100

    DualSourceCubicChunksColumnData now uses relocatingConfig properly

--- src/main/java/cubicchunks/converter/lib/convert/cc2ccmerging/CC2CCDualSourceMergingDataConverter.java
+++ src/main/java/cubicchunks/converter/lib/convert/cc2ccmerging/CC2CCDualSourceMergingDataConverter.java
@@ -1,30 +1,46 @@
 package cubicchunks.converter.lib.convert.cc2ccmerging;
 
+import cubicchunks.converter.lib.conf.ConverterConfig;
 import cubicchunks.converter.lib.convert.ChunkDataConverter;
 import cubicchunks.converter.lib.convert.data.CubicChunksColumnData;
 import cubicchunks.converter.lib.convert.data.DualSourceCubicChunksColumnData;
+import cubicchunks.converter.lib.util.EditTask;
 
 import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class CC2CCDualSourceMergingDataConverter implements ChunkDataConverter<DualSourceCubicChunksColumnData, CubicChunksColumnData> {
+    private final List<EditTask> relocateTasks;
+
+    @SuppressWarnings("unchecked")
+    public CC2CCDualSourceMergingDataConverter(ConverterConfig config) {
+        relocateTasks = (List<EditTask>) config.getValue("relocations");
+    }
+
     @Override public Set<CubicChunksColumnData> convert(DualSourceCubicChunksColumnData input) {
         Map<Integer, ByteBuffer> priorityCubes = input.getPriorityCubeData();
         Map<Integer, ByteBuffer> fallbackCubes = input.getFallbackCubeData();
-        Map<Integer, ByteBuffer> cubes = new HashMap<>();
+        Map<Integer, ByteBuffer> inCubes = new HashMap<>();
 
         if(priorityCubes != null)
-            priorityCubes.forEach(cubes::put);
+            priorityCubes.forEach(inCubes::put);
         fallbackCubes.forEach((y, cube) -> {
-            if(!cubes.containsKey(y))
-                cubes.put(y, cube);
+            if(!inCubes.containsKey(y))
+                inCubes.put(y, cube);
+        });
+
+        Map<Integer, ByteBuffer> outCubes = new HashMap<>();
+        inCubes.forEach((y, cube) -> {
+            for (EditTask relocateTask : relocateTasks) {
+                if(relocateTask.getSourceBox().intersects(input.getPosition().getEntryX(), y, input.getPosition().getEntryZ())) {
+                    outCubes.put(y, cube);
+                    break;
+                }
+            }
         });
 
         Set<CubicChunksColumnData> columnData = new HashSet<>();
-        columnData.add(new CubicChunksColumnData(input.getDimension(), input.getPosition(), input.getColumnData(), cubes));
+        columnData.add(new CubicChunksColumnData(input.getDimension(), input.getPosition(), input.getColumnData(), outCubes));
         return columnData;
     }
 }
