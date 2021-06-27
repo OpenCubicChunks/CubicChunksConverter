00c03d1c1bd489a864f7b1003b8ecc53588c6af1
DaPorkchop_ <daporkchop@daporkchop.net>
2021-06-27 13:13:01 +0200

    make GuiMain not hang the main thread

--- src/main/java/cubicchunks/converter/gui/GuiMain.java
+++ src/main/java/cubicchunks/converter/gui/GuiMain.java
@@ -30,12 +30,11 @@ import java.awt.GraphicsEnvironment;
 
 public class GuiMain {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         if ((args.length > 0 && args[0].equals("--headless")) || GraphicsEnvironment.isHeadless()) {
             HeadlessMain.main(args);
             return;
         }
         EventQueue.invokeLater(() -> new GuiFrame().init());
-        Thread.sleep(Long.MAX_VALUE);
     }
 }
