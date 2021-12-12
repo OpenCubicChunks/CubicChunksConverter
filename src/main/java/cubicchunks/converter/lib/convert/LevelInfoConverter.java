8891e803275cf408d2104a8bbdc4d43f00a0f8a3
Tom Martin <tom.martin1239@gmail.com>
2021-12-12 01:21:31 +0000

    update license to 2021

--- src/main/java/cubicchunks/converter/lib/convert/LevelInfoConverter.java
+++ src/main/java/cubicchunks/converter/lib/convert/LevelInfoConverter.java
@@ -1,7 +1,7 @@
 /*
  *  This file is part of CubicChunksConverter, licensed under the MIT License (MIT).
  *
- *  Copyright (c) 2017 contributors
+ *  Copyright (c) 2017-2021 contributors
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
