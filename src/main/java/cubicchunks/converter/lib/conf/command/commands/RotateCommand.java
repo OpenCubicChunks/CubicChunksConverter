e42d6c6492b7e581c22826dca0510f667ca656e2
gcarvellas <gcarvellas@castlepointanime.com>
2022-08-02 16:31:23 -0400

    created rotate command

---
+++ src/main/java/cubicchunks/converter/lib/conf/command/commands/RotateCommand.java
@@ -0,0 +1,52 @@
+/*
+ *  This file is part of CubicChunksConverter, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2017-2021 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package cubicchunks.converter.lib.conf.command.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import cubicchunks.converter.lib.conf.command.EditTaskContext;
+import cubicchunks.converter.lib.conf.command.arguments.BoundingBoxArgument;
+import cubicchunks.converter.lib.conf.command.arguments.Vector3iArgument;
+import cubicchunks.converter.lib.util.BoundingBox;
+import cubicchunks.converter.lib.util.Vector3i;
+import cubicchunks.converter.lib.util.edittask.RotateEditTask;
+
+public class RotateCommand {
+    public static void register(CommandDispatcher<EditTaskContext> dispatcher) {
+        dispatcher.register(LiteralArgumentBuilder.<EditTaskContext>literal("rotate")
+                .then(RequiredArgumentBuilder.<EditTaskContext, Vector3i>argument("origin", new Vector3iArgument())
+                        .then(RequiredArgumentBuilder.<EditTaskContext, Integer>argument("degrees", IntegerArgumentType.integer(-360, 360))
+                        .executes((info) -> {
+                            info.getSource().addEditTask(new RotateEditTask(
+                                    info.getArgument("origin", Vector3i.class),
+                                    IntegerArgumentType.getInteger(info, "degrees"))
+                            );
+                            return 1;
+                        })
+                ))
+        );
+    }
+}
