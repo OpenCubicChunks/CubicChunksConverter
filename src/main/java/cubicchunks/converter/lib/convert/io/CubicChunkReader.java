OpenCubicChunks/CubicChunksConverter

commit eb9b8f71a193580f7b950c84ef3f62b55ee53c17

Add schematic import support for relocating converter

src/main/java/cubicchunks/converter/lib/convert/io/CubicChunkReader.java
@@ -58,9 +58,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.WeakHashMap;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -75,21 +77,28 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
     private static final Map<SaveCubeColumns, List<IRegionProvider<EntryLocation2D>>> providers2d = new WeakHashMap<>();
     private static final Map<SaveCubeColumns, List<IRegionProvider<EntryLocation3D>>> providers3d = new WeakHashMap<>();
 
-    private final List<BoundingBox> regionBoundingBoxes;
+    private final Map<String, List<BoundingBox>> regionBoundingBoxes = new HashMap<>();
+    private final Map<String, List<BoundingBox>> createIfMissingBoxes = new HashMap<>();
 
     public CubicChunkReader(Path srcDir, ConverterConfig config) {
         super(srcDir, (dim, path) -> Files.exists(getDimensionPath(dim, path)) ? createSave(getDimensionPath(dim, path)) : null);
         loadThread = Thread.currentThread();
         if(config.hasValue("relocations")) {
-            this.regionBoundingBoxes = new ArrayList<>();
             @SuppressWarnings("unchecked") List<EditTask> tasks = (List<EditTask>) config.getValue("relocations");
 
             for (EditTask task : tasks) {
-                task.getSrcBoxes().forEach(box -> regionBoundingBoxes.add(box.asRegionCoords(new Vector3i(16, 16, 16))));
-                task.getDstBoxes().forEach(box -> regionBoundingBoxes.add(box.asRegionCoords(new Vector3i(16, 16, 16))));
+                String dim = task.getDimension();
+                regionBoundingBoxes.computeIfAbsent(dim, x -> new ArrayList<>());
+                createIfMissingBoxes.computeIfAbsent(dim, x -> new ArrayList<>());
+
+                List<BoundingBox> srcBoxes = task.getSrcBoxes();
+                srcBoxes.forEach(box -> regionBoundingBoxes.get(dim).add(box.asRegionCoords(new Vector3i(16, 16, 16))));
+                task.getDstBoxes().forEach(box -> regionBoundingBoxes.get(dim).add(box.asRegionCoords(new Vector3i(16, 16, 16))));
+                if (task.createSrcCubesIfMissing()) {
+                    this.createIfMissingBoxes.get(dim).addAll(srcBoxes);
+                }
             }
-        } else
-            regionBoundingBoxes = null;
+        }
     }
 
     private static Path getDimensionPath(Dimension d, Path worldDir) {
@@ -118,6 +127,12 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
 
             List<IRegionProvider<EntryLocation3D>> regionProviders = providers3d.get(save);
 
+            // TODO: efficient hashset for this
+            Set<Vector3i> toCreateIfMissing = new HashSet<>();
+            List<BoundingBox> createIfMissingList = createIfMissingBoxes.get(dim.getDirectory());
+            if (createIfMissingList != null) {
+                createIfMissingList.forEach(box -> box.forEach(toCreateIfMissing::add));
+            }
             CheckedConsumer<EntryLocation3D, IOException> cons = interruptibleConsumer(loc -> {
                 EntryLocation2D loc2d = new EntryLocation2D(loc.getEntryX(), loc.getEntryZ());
                 chunksMap.computeIfAbsent(loc2d, l -> {
@@ -126,7 +141,12 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                     chunks.add(new AbstractMap.SimpleEntry<>(loc2d, arr));
                     return arr;
                 }).add(loc.getEntryY());
+                if (!toCreateIfMissing.isEmpty()) {
+                    toCreateIfMissing.remove(new Vector3i(loc.getEntryX(), loc.getEntryY(), loc.getEntryZ()));
+                }
             });
+
+            List<BoundingBox> regionBoundingBoxList = regionBoundingBoxes.get(dim.getDirectory());
 
             for (int i = 0; i < regionProviders.size(); i++) {
                 IRegionProvider<EntryLocation3D> p = regionProviders.get(i);
@@ -136,8 +156,8 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                         Vector3i regionPos = toRegionPos(key);
                         boolean filtered = true;
                         try {
-                            if(regionBoundingBoxes != null) {
-                                for (BoundingBox regionBox: regionBoundingBoxes) {
+                            if(regionBoundingBoxList != null) {
+                                for (BoundingBox regionBox: regionBoundingBoxList) {
                                     if (regionBox.intersects(regionPos.getX(), regionPos.getY(), regionPos.getZ())) {
                                         filtered = false;
                                     }
@@ -159,8 +179,8 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                         Vector3i regionPos = toRegionPos(regionKey);
                         boolean filtered = true;
 
-                        if(regionBoundingBoxes != null) {
-                            for (BoundingBox regionBox: regionBoundingBoxes) {
+                        if(regionBoundingBoxList != null) {
+                            for (BoundingBox regionBox: regionBoundingBoxList) {
                                 if (regionBox.intersects(regionPos.getX(), regionPos.getY(), regionPos.getZ())) {
                                     filtered = false;
                                 }
@@ -188,6 +208,9 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                         reg.close();
                     });
                 }
+            }
+            for (Vector3i vector3i : toCreateIfMissing) {
+                cons.accept(new EntryLocation3D(vector3i.getX(), vector3i.getY(), vector3i.getZ()));
             }
         }
         return dimensions;
@@ -237,8 +260,8 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                     int y = yCursor.value;
                     ByteBuffer cube;
                     try {
-                        cube = save.load(new EntryLocation3D(pos2d.getEntryX(), y, pos2d.getEntryZ()), true).orElseThrow(
-                                () -> new IllegalStateException("Expected cube at " + pos2d + " at y=" + y + " in dimension " + dim));
+                        EntryLocation3D location = new EntryLocation3D(pos2d.getEntryX(), y, pos2d.getEntryZ());
+                        cube = save.load(location, true).orElse(Utils.createAirCubeBuffer(location));
                     } catch (Exception e) {
                         e.printStackTrace();
                         if (!errorHandler.test(e)) {
@@ -250,7 +273,6 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                 }
                 CubicChunksColumnData data = new CubicChunksColumnData(dim, pos2d, column, cubes);
                 consumer.accept(data);
-
             });
         }
     }

src/main/java/cubicchunks/converter/lib/convert/io/PriorityCubicChunkReader.java
@@ -62,21 +62,28 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
     private static final Map<SaveCubeColumns, List<IRegionProvider<EntryLocation2D>>> providers2d = new WeakHashMap<>();
     private static final Map<SaveCubeColumns, List<IRegionProvider<EntryLocation3D>>> providers3d = new WeakHashMap<>();
 
-    private final List<BoundingBox> regionBoundingBoxes;
+    private final Map<String, List<BoundingBox>> regionBoundingBoxes = new HashMap<>();
+    private final Map<String, List<BoundingBox>> createIfMissingBoxes = new HashMap<>();
 
     public PriorityCubicChunkReader(Path srcDir, ConverterConfig config) {
         super(srcDir, (dim, path) -> Files.exists(getDimensionPath(dim, path)) ? createSave(getDimensionPath(dim, path)) : null);
         loadThread = Thread.currentThread();
         if(config.hasValue("relocations")) {
-            this.regionBoundingBoxes = new ArrayList<>();
             @SuppressWarnings("unchecked") List<EditTask> tasks = (List<EditTask>) config.getValue("relocations");
 
             for (EditTask task : tasks) {
-                task.getSrcBoxes().forEach(box -> regionBoundingBoxes.add(box.asRegionCoords(new Vector3i(16, 16, 16))));
-                task.getDstBoxes().forEach(box -> regionBoundingBoxes.add(box.asRegionCoords(new Vector3i(16, 16, 16))));
+                String dim = task.getDimension();
+                regionBoundingBoxes.computeIfAbsent(dim, x -> new ArrayList<>());
+                createIfMissingBoxes.computeIfAbsent(dim, x -> new ArrayList<>());
+
+                List<BoundingBox> srcBoxes = task.getSrcBoxes();
+                srcBoxes.forEach(box -> regionBoundingBoxes.get(dim).add(box.asRegionCoords(new Vector3i(16, 16, 16))));
+                task.getDstBoxes().forEach(box -> regionBoundingBoxes.get(dim).add(box.asRegionCoords(new Vector3i(16, 16, 16))));
+                if (task.createSrcCubesIfMissing()) {
+                    this.createIfMissingBoxes.get(dim).addAll(srcBoxes);
+                }
             }
-        } else
-            regionBoundingBoxes = null;
+        }
     }
 
     private static Path getDimensionPath(Dimension d, Path worldDir) {
@@ -105,7 +112,14 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
 
             List<IRegionProvider<EntryLocation3D>> regionProviders = providers3d.get(save);
 
-            CheckedConsumer<EntryLocation3D, IOException> cons = interruptibleConsumer(loc -> {
+            // TODO: efficient hashset for this
+            Set<Vector3i> toCreateIfMissing = new HashSet<>();
+            List<BoundingBox> createIfMissingList = createIfMissingBoxes.get(dim.getDirectory());
+            if (createIfMissingList != null) {
+                createIfMissingList.forEach(box -> box.forEach(toCreateIfMissing::add));
+            }
+
+            CheckedConsumer<EntryLocation3D, IOException> addToMap = interruptibleConsumer(loc -> {
                 EntryLocation2D loc2d = new EntryLocation2D(loc.getEntryX(), loc.getEntryZ());
                 chunksMap.computeIfAbsent(loc2d, l -> {
                     increment.run();
@@ -115,6 +129,15 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                 }).add(loc.getEntryY());
             });
 
+            CheckedConsumer<EntryLocation3D, IOException> addFromDisk = interruptibleConsumer(loc -> {
+                addToMap.accept(loc);
+                if (!toCreateIfMissing.isEmpty()) {
+                    toCreateIfMissing.remove(new Vector3i(loc.getEntryX(), loc.getEntryY(), loc.getEntryZ()));
+                }
+            });
+
+            List<BoundingBox> regionBoundingBoxList = regionBoundingBoxes.get(dim.getDirectory());
+
             for (int i = 0; i < regionProviders.size(); i++) {
                 IRegionProvider<EntryLocation3D> p = regionProviders.get(i);
                 if (i == 0) {
@@ -123,8 +146,8 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                         Vector3i regionPos = toRegionPos(key);
                         boolean filtered = true;
                         try {
-                            if(regionBoundingBoxes != null) {
-                                for (BoundingBox regionBox: regionBoundingBoxes) {
+                            if(regionBoundingBoxList != null) {
+                                for (BoundingBox regionBox: regionBoundingBoxList) {
                                     if (regionBox.intersects(regionPos.getX(), regionPos.getY(), regionPos.getZ())) {
                                         filtered = false;
                                     }
@@ -133,7 +156,7 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                                 filtered = false;
                             }
                             if(!filtered) {
-                                reg.forEachKey(cons);
+                                reg.forEachKey(addFromDisk);
                                 reg.close();
                             }
                         } catch (IOException e) {
@@ -146,8 +169,8 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                         Vector3i regionPos = toRegionPos(regionKey);
                         boolean filtered = true;
 
-                        if(regionBoundingBoxes != null) {
-                            for (BoundingBox regionBox: regionBoundingBoxes) {
+                        if(regionBoundingBoxList != null) {
+                            for (BoundingBox regionBox: regionBoundingBoxList) {
                                 if (regionBox.intersects(regionPos.getX(), regionPos.getY(), regionPos.getZ())) {
                                     filtered = false;
                                 }
@@ -170,11 +193,15 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                                     return;
                                 }
                             }
-                            cons.accept(key);
+                            addFromDisk.accept(key);
                         });
                         reg.close();
                     });
                 }
+            }
+
+            for (Vector3i vector3i : toCreateIfMissing) {
+                addToMap.accept(new EntryLocation3D(vector3i.getX(), vector3i.getY(), vector3i.getZ()));
             }
         }
         return dimensions;
@@ -213,8 +240,8 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                             return;
                         }
                         int y = yCursor.value;
-                        ByteBuffer cube = save.load(new EntryLocation3D(pos2d.getEntryX(), y, pos2d.getEntryZ()), true).orElseThrow(
-                                () -> new IllegalStateException("Expected cube at " + pos2d + " at y=" + y + " in dimension " + dim));
+                        EntryLocation3D location = new EntryLocation3D(pos2d.getEntryX(), y, pos2d.getEntryZ());
+                        ByteBuffer cube = save.load(location, true).orElse(Utils.createAirCubeBuffer(location));
                         cubes.put(y, new ImmutablePair<>(0L, cube));
                     }
                     PriorityCubicChunksColumnData data = new PriorityCubicChunksColumnData(dim, pos2d, column, cubes, true);

src/main/java/cubicchunks/converter/lib/conf/command/EditTaskCommands.java
@@ -46,6 +46,7 @@ public class EditTaskCommands {
         KeepCommand.register(dispatcher);
         SetCommand.register(dispatcher);
         ReplaceCommand.register(dispatcher);
+        SchematicCommand.register(dispatcher);
         ConfigCommand.register(dispatcher);
     }
 

src/main/java/cubicchunks/converter/lib/conf/command/arguments/Matrix4dArgument.java
@@ -1,0 +1,164 @@
+/*
+ *  This file is part of CubicChunksConverter, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2017-2021 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package cubicchunks.converter.lib.conf.command.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import cubicchunks.converter.lib.util.Matrix4d;
+
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+public class Matrix4dArgument implements ArgumentType<Matrix4d> {
+    @Override
+    public Matrix4d parse(StringReader reader) throws CommandSyntaxException {
+        return parseVector(reader);
+    }
+
+    public static Matrix4d parseVector(StringReader reader) throws CommandSyntaxException {
+        int i = reader.getCursor();
+        double m00 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m01 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m02 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m03 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+
+
+        double m10 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m11 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m12 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m13 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+
+
+        double m20 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m21 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m22 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m23 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+
+
+        double m30 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m31 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m32 = parseDouble(reader);
+        if(!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(i);
+            throw new RuntimeException("Incomplete or invalid command!");
+        }
+        reader.skip();
+        double m33 = parseDouble(reader);
+        return new Matrix4d(
+            m00, m01, m02, m03,
+            m10, m11, m12, m13,
+            m20, m21, m22, m23,
+            m30, m31, m32, m33
+        );
+    }
+
+    public static double parseDouble(StringReader reader) throws CommandSyntaxException {
+        if (!reader.canRead()) {
+            throw new RuntimeException("Expected integer got end of command!");
+        } else {
+            return reader.readDouble();
+        }
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getExamples() {
+        return null;
+    }
+}

src/main/java/cubicchunks/converter/lib/conf/command/commands/SchematicCommand.java
@@ -1,0 +1,120 @@
+/*
+ *  This file is part of CubicChunksConverter, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2017-2021 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package cubicchunks.converter.lib.conf.command.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import cubicchunks.converter.lib.conf.command.EditTaskContext;
+import cubicchunks.converter.lib.conf.command.arguments.Matrix4dArgument;
+import cubicchunks.converter.lib.conf.command.arguments.Vector3iArgument;
+import cubicchunks.converter.lib.util.Matrix4d;
+import cubicchunks.converter.lib.util.Schematic;
+import cubicchunks.converter.lib.util.Vector3i;
+import cubicchunks.converter.lib.util.edittask.SchematicTask;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SchematicCommand {
+    public static void register(CommandDispatcher<EditTaskContext> dispatcher) {
+        dispatcher.register(LiteralArgumentBuilder.<EditTaskContext>literal("schematic")
+            .then(RequiredArgumentBuilder.<EditTaskContext, String>argument("path", StringArgumentType.string())
+                .then(LiteralArgumentBuilder.<EditTaskContext>literal("inDimension")
+                    .then(RequiredArgumentBuilder.<EditTaskContext, String>argument("dimension", StringArgumentType.word())
+                        .then(LiteralArgumentBuilder.<EditTaskContext>literal("at")
+                            .then(RequiredArgumentBuilder.<EditTaskContext, Vector3i>argument("position", new Vector3iArgument())
+                                .executes(SchematicCommand::executeWithPosition)
+                                .then(LiteralArgumentBuilder.<EditTaskContext>literal("skipAir")
+                                    .executes(SchematicCommand::executeWithPositionSkipAir))
+                            )
+                        )
+                        .then(LiteralArgumentBuilder.<EditTaskContext>literal("transform")
+                            .then(RequiredArgumentBuilder.<EditTaskContext, Matrix4d>argument("matrix", new Matrix4dArgument())
+                                .executes(SchematicCommand::executeWithTransform)
+                                .then(LiteralArgumentBuilder.<EditTaskContext>literal("skipAir")
+                                    .executes(SchematicCommand::executeWithTransformSkipAir))
+                            )
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    private static int executeWithPositionSkipAir(CommandContext<EditTaskContext> ctx) {
+        return executeWithPosition(ctx, true);
+    }
+
+    private static int executeWithTransformSkipAir(CommandContext<EditTaskContext> ctx) {
+        return executeWithTransform(ctx, true);
+    }
+
+    private static int executeWithTransform(CommandContext<EditTaskContext> info) {
+        return executeWithTransform(info, false);
+    }
+
+    private static int executeWithPosition(CommandContext<EditTaskContext> info) {
+        return executeWithPosition(info, false);
+    }
+
+    private static int executeWithTransform(CommandContext<EditTaskContext> info, boolean skipAir) {
+        Path path = Paths.get(StringArgumentType.getString(info, "path"));
+        Matrix4d transform = info.getArgument("matrix", Matrix4d.class);
+        String dimension = StringArgumentType.getString(info, "dimension");
+        if (dimension.equals("0")) {
+            dimension = "";
+        }
+        try {
+            info.getSource().addEditTask(new SchematicTask(
+                Schematic.load(path), transform, skipAir, dimension
+            ));
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return 1;
+    }
+
+    // TODO: add dimension specification in a generalized way?
+    private static int executeWithPosition(CommandContext<EditTaskContext> info, boolean skipAir) {
+        Path path = Paths.get(StringArgumentType.getString(info, "path"));
+        Vector3i position = info.getArgument("position", Vector3i.class);
+        String dimension = StringArgumentType.getString(info, "dimension");
+        if (dimension.equals("0")) {
+            dimension = "";
+        }
+        Matrix4d transform = new Matrix4d().setIdentity().translate(position);
+        try {
+            info.getSource().addEditTask(new SchematicTask(
+                Schematic.load(path), transform, skipAir, dimension
+            ));
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+        return 1;
+    }
+}

commit 818ad7afea191b2d4fff982b653dd5e62ca86c33

Fix massive performance regression in eb9b8f71 Due to always creating air cubes even if unneeded

src/main/java/cubicchunks/converter/lib/convert/io/CubicChunkReader.java
@@ -268,7 +268,7 @@ public class CubicChunkReader extends BaseMinecraftReader<CubicChunksColumnData,
                     ByteBuffer cube;
                     try {
                         EntryLocation3D location = new EntryLocation3D(pos2d.getEntryX(), y, pos2d.getEntryZ());
-                        cube = save.load(location, true).orElse(Utils.createAirCubeBuffer(location));
+                        cube = save.load(location, true).orElseGet(() -> Utils.createAirCubeBuffer(location));
                     } catch (Exception e) {
                         e.printStackTrace();
                         if (!errorHandler.test(e)) {

src/main/java/cubicchunks/converter/lib/convert/io/PriorityCubicChunkReader.java
@@ -247,7 +247,7 @@ public class PriorityCubicChunkReader extends BaseMinecraftReader<PriorityCubicC
                         }
                         int y = yCursor.value;
                         EntryLocation3D location = new EntryLocation3D(pos2d.getEntryX(), y, pos2d.getEntryZ());
-                        ByteBuffer cube = save.load(location, true).orElse(Utils.createAirCubeBuffer(location));
+                        ByteBuffer cube = save.load(location, true).orElseGet(() -> Utils.createAirCubeBuffer(location));
                         cubes.put(y, new ImmutablePair<>(0L, cube));
                     }
                     PriorityCubicChunksColumnData data = new PriorityCubicChunksColumnData(dim, pos2d, column, cubes, true);